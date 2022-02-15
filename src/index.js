10b9140193f4498a6bd8ee7c03b865ae7736af90
Tatiana <amora4ka@gmail.com>
2022-02-15 18:01:09 +0300

    feat: resolve some cases

--- src/index.js
+++ src/index.js
@@ -1,3 +1,49 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  if (str.length % 2 > 0){
+    return false;
+  }
+
+  const arrFromStr = Array.from (str);
+  
+  for (el of bracketsConfig) {
+    if (el[0] === el[1]) {
+      const arrWithEqBrackets = arrFromStr.filter( x => x === el[0])
+      if (arrWithEqBrackets.length % 2 > 0) {
+        return false;
+      }
+    }  else { 
+      const arrWithOpen = arrFromStr.filter( x => x === el[0]);
+      const arrWithClose = arrFromStr.filter( x => x === el[1]);
+
+      if (arrWithOpen.length !== arrWithClose.length) {
+        return false;
+      }
+
+      if (arrFromStr.lastIndexOf(el[0]) > arrFromStr.lastIndexOf(el[1])) {
+        return false;
+      }
+
+      do {
+        let inOpen = arrFromStr.lastIndexOf(el[0]);
+        let arrFromOpen = arrFromStr.slice(inOpen);
+        if (!arrFromStr.slice(inOpen).includes(el[1])) {
+          return false;
+        }
+        let inClose = arrFromOpen.indexOf(el[1]) + inOpen;
+        
+        if (arrFromStr.slice(inOpen+1, inClose).length % 2 > 0) {
+          return false;
+        }
+        arrFromStr.splice(inOpen, 1);
+        arrFromStr.splice(inClose, 1);
+      
+      } while (arrFromStr.includes(el[0]))
+
+      arrFromStr.splice(0).concat(Array.from (str));
+    
+  }
+  arrFromStr.splice(0).concat(Array.from (str));
+}
+
+  return true;
 }
